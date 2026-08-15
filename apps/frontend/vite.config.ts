35a3f0436cb25ab0285793c6445700514cd3e1f6
Tom <tyohin2016@163.com>
2026-08-15 12:20:43 +1200

    fix the issue that page is blank when open github pages

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+    // GitHub Pages 部署路径
+  base: '/AI-Career-Management-Platform/',
+  
   plugins: [react()],
   resolve: {
     alias: {
